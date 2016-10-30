bbd4df6ab60007e6f7169437aae8e82f3611e90f
Gabriel Silk <gabesilk@gmail.com>
2016-10-29 19:20:08 -0700

    simplify test runner

--- test/mocha_runner.js
+++ test/mocha_runner.js
@@ -1,14 +1,2 @@
-/* global suite:false, test:false */
-
-// const jsdom = require('jsdom').jsdom;
-// const mochaBrackets = require('mocha-brackets');
-
-// const document = () => jsdom('<div id="main"></div>');
-// const el = () => document().getElementById('main');
-
-// mochaBrackets.load({ document, el }, require('./test'));
-
-
-const mochaBrackets = require('mocha-brackets');
-
-mochaBrackets.load({}, require('./test'));
+require('mocha-brackets')
+  .load({}, require('./test'));
