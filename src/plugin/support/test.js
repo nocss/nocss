38cd7a5f1135baed25e6254b29cd071880ee278f
Gabriel Silk <gabesilk@gmail.com>
2016-10-29 19:32:21 -0700

    reorg support-level tests

--- src/plugin/support/test.js
+++ src/plugin/support/test.js
@@ -4,48 +4,45 @@ const sjss = require('../../index')();
 sjss.use(require('./index'));
 
 module.exports = [
-
   ['Support levels',
-
-   ['Invalid value null', () => {
-     assert.throws(_.partial(sjss.render, {
-       'p': {
-         'white-space': null,
-       },
-     }));
-   }],
-
-   ['Invalid value string', () => {
-     assert.throws(_.partial(sjss.render, {
-       'p': {
-         'white-space': 'hello',
-       },
-     }));
-   }],
-
-   ['Invalid numeral', () => {
-     assert.throws(_.partial(sjss.render, {
-       'p': {
-         'orphans': '5.4',
-       },
-     }));
-   }],
-
-   ['Valid aliased numeral', () => {
-     assert.equal(sjss.render({
-       'p': {
-         'widows': '5',
-       },
-     }), 'p {\n  widows: 5;\n}');
-   }],
-
-   ['Invalid aliased value', () => {
-     assert.throws(_.partial(sjss.render, {
-       'p': {
-         'widows': 'nada',
-       },
-     }));
-   }],
+   ['Value checking',
+    ['Invalid value null', () => {
+      assert.throws(_.partial(sjss.render, {
+        'p': {
+          'white-space': null,
+        },
+      }));
+    }],
+    ['Invalid value string', () => {
+      assert.throws(_.partial(sjss.render, {
+        'p': {
+          'white-space': 'hello',
+        },
+      }));
+    }],
+    ['Invalid numeral', () => {
+      assert.throws(_.partial(sjss.render, {
+        'p': {
+          'orphans': '5.4',
+        },
+      }));
+    }],
+   ],
+   ['Aliasing',
+    ['Valid aliased numeral', () => {
+      assert.equal(sjss.render({
+        'p': {
+          'widows': '5',
+        },
+      }), 'p {\n  widows: 5;\n}');
+    }],
+    ['Invalid aliased value', () => {
+      assert.throws(_.partial(sjss.render, {
+        'p': {
+          'widows': 'nada',
+        },
+      }));
+    }],
+   ],
   ],
-
 ];
